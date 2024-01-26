dd594da5850727a673ff4ee3ef02553e1504769c
DewanggaMz <dewanggamahfuz56@gmail.com>
2024-01-27 01:17:52 +0700

    first commit

---
+++ public/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js"
+const firebaseConfig = {
+	apiKey: "AIzaSyAvS5Gmz--_BgACy3DRMPITQY4h9tLWydQ",
+	authDomain: "cat-fish-8617a.firebaseapp.com",
+	databaseURL:
+		"https://cat-fish-8617a-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "cat-fish-8617a",
+	storageBucket: "cat-fish-8617a.appspot.com",
+	messagingSenderId: "436684278951",
+	appId: "1:436684278951:web:1d521d51cb4af82bc35f82",
+}
+
+const app = initializeApp(firebaseConfig)
